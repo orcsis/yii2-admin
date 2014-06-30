b6ef22151a10a0223384e6dbb71c6b6d4305a166
orcoliver <orcoliver@gmail.com>
2014-06-30 10:14:32 -0430

    Cambio de Nombre de Campos y tabla en migración.

--- migration/m140602_111327_create_menu_table.php
+++ migration/m140602_111327_create_menu_table.php
@@ -7,19 +7,21 @@ class m140602_111327_create_menu_table extends \yii\db\Migration
 
     public function safeUp()
     {
-        $this->createTable('{{%menu}}', [
-            'id' => Schema::TYPE_PK,
-            'name' => Schema::TYPE_STRING . '(128) NOT NULL',
-            'parent' => Schema::TYPE_INTEGER. ' NULL',
-            'route' => Schema::TYPE_STRING . '(256)',
-            'order' => Schema::TYPE_INTEGER,
-            'data' => Schema::TYPE_TEXT,
-            'FOREIGN KEY (parent) REFERENCES {{%menu}}(id) ON DELETE SET NULL ON UPDATE CASCADE',
+        $this->createTable('{{%osmenu}}', [
+            'men_id' => Schema::TYPE_PK,
+            'men_nombre' => Schema::TYPE_STRING . '(128) NOT NULL',
+            'men_parent' => Schema::TYPE_INTEGER. ' NULL',
+            'men_descri' => Schema::TYPE_STRING . '(256)',
+			'men_modulo' => Schema::TYPE_STRING . '(128) NOT NULL',
+			'men_url' => Schema::TYPE_STRING . '(128) NOT NULL',
+            'men_orden' => Schema::TYPE_INTEGER,
+            'men_data' => Schema::TYPE_TEXT,
+            'FOREIGN KEY (men_parent) REFERENCES {{%osmenu}}(men_id) ON DELETE SET NULL ON UPDATE CASCADE',
         ]);
     }
 
     public function safeDown()
     {
-        $this->dropTable('{{%menu}}');
+        $this->dropTable('{{%osmenu}}');
     }
 }
